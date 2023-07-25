5f885c929b87165826cea3e2fa940b6f7392b976
Jinbin <jcao0515@gmail.com>
2023-07-25 01:05:10 -0700

    setup project

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+//entry file for app
+import express, { Express } from 'express';
+import { ChattyServer } from './setupServer';
+import databaseConnection from './setupDatabase';
+import { config } from './config';
+
+class Application {
+  public initialize(): void {
+    databaseConnection();
+    const app: Express = express();
+    const server: ChattyServer = new ChattyServer(app);
+
+    server.start();
+  }
+
+  private localConfig(): void {
+    config.validateConfig();
+  }
+}
+
+const application: Application = new Application();
+application.initialize();
